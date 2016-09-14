9ff61e76680842477071190abb60dfeeb7414037
Dmitri Lapin <lapin@cvisionlab.com>
2016-09-14 12:38:25 +0300

    Fixed quotes

--- git_setup.sh
+++ git_setup.sh
@@ -3,5 +3,5 @@
 BASEPATH=$(dirname $0)
 cp -n $BASEPATH/git_version.txt version.txt
 cp -n $BASEPATH/version.in version.in
-echo \"version.txt export-subst\" >> .gitattributes
+echo "version.txt export-subst" >> .gitattributes
 echo "Don't forget to commit changes"
